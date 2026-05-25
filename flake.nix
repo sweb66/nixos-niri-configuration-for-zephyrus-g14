30c316745eb13bfa207d32b1d0e3aacda4b08e40
sweb66 <norna6959@gmail.com>
2026-05-26 01:39:50 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,14 @@
   description = "ASUS Zephyrus G14 GA401IU + Niri";
 
   inputs = {
-    # Основной репозиторий NixOS 25.11
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    
-    # Профили железа
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    # Niri compositor
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Home Manager для пользовательских настроек
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -31,13 +26,8 @@
       inherit system;
       specialArgs = { inherit inputs user; };
       modules = [
-        # Профиль железа для твоей модели
         nixos-hardware.nixosModules.asus-zephyrus-ga401
-        
-        # Niri из флейка
         niri.nixosModules.niri
-        
-        # Home Manager
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
@@ -45,8 +35,6 @@
           home-manager.users.${user} = import ./home.nix;
           home-manager.extraSpecialArgs = { inherit inputs user; };
         }
-        
-        # Основная конфигурация системы
         ./configuration.nix
       ];
     };
